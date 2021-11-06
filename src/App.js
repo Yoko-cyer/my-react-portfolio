fa5eff6e9e3cf39072fde1ee631c4136c59ebcea
Yoko <yokoujihara622@gmail.com>
2021-11-06 16:04:15 +1000

    added particle animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar';
+import ParticleAnimation from 'react-particle-animation'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <ParticleAnimation 
+        style={{ height: '96vh'}}
+        background={{ r: 21, g: 22, b: 23, a: 1 }}
+        particleSpeed={0.5}
+        particleRadius={1}
+        color={{ r: 250, g: 217, b: 249, a: 255 }}
+      />
     </div>
   );
 }
