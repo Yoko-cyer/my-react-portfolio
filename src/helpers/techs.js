d54ee557772817b909ce0ba771613702fb93a484
Yoko <yokoujihara622@gmail.com>
2021-11-08 17:28:51 +1000

    added technologies

---
+++ src/helpers/techs.js
@@ -0,0 +1,54 @@
+export const techs = [
+  {
+    name: 'HTML 5',
+    iconName: 'html5'
+  },
+  {
+    name: 'CSS 3',
+    iconName: 'css3'
+  },
+  {
+    name: 'Javascript',
+    iconName: 'javascript'
+  },
+  {
+    name: 'React',
+    iconName: 'react'
+  },
+  {
+    name: 'Ruby',
+    iconName: 'ruby'
+  },
+  {
+    name: 'Ruby on Rails',
+    iconName: 'ruby_on_rails'
+  },
+  {
+    name: 'Git',
+    iconName: 'git'
+  },
+  {
+    name: 'Github',
+    iconName: 'github'
+  },
+  {
+    name: 'Trello',
+    iconName: 'trello'
+  },
+  {
+    name: 'Sass',
+    iconName: 'sass'
+  },
+  {
+    name: 'VS code',
+    iconName: 'vscode'
+  },
+  {
+    name: 'Bootstrap',
+    iconName: 'bootstrap'
+  },
+  {
+    name: 'Postresql',
+    iconName: 'postresql'
+  },
+]
